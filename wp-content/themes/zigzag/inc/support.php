62eafec2931fec3b3844e7c8dd3052c2c4703365
AdrienZ <adrienzaganelli@gmail.com>
2020-07-13 12:17:54 +0200

    mu plugins

--- wp-content/themes/zigzag/inc/support.php
+++ wp-content/themes/zigzag/inc/support.php
@@ -9,10 +9,13 @@ function my_theme_supports() {
   // post thumbnails
   add_theme_support('post-thumbnails'); // can tell wich type to add in second argument (post, etc...)
 
+  add_theme_support('custom-logo');
+  add_theme_support('html5');
 
+  // rss
+  add_theme_support( 'automatic-feed-links' );
 
   // editable menus
-  add_theme_support('custom-logo');
   add_theme_support('menus');
   register_nav_menu('Header', 'Menu principal');
 }
